d4f85e35a6775363bf1598b17a546bd96e1feb68
Samiul Anwar <samiul.anwar@gmail.com>
2017-09-09 21:38:25 -0400

    added some stuff to js file

--- simon.js
+++ simon.js
@@ -1,1 +1,26 @@
 console.log('hello world');
+
+//globals here
+var isItOn=false;
+
+function powerButton(){
+	//powers the game on and off
+	if(isItOn){
+		return isItOn=false;
+	}
+	else{
+		return isItOn=true;
+	}
+}
+
+function moveSeries(moveLength){
+	var moveList=[]
+	for(var i=0;i<moveLength;i++){
+		moveList.push(Math.floor((Math.random() * 4) + 1));
+	}
+	return moveList;
+}
+
+function areTheyEqual(aiMoves,userMoves){
+	//compare two arrays and return true or false if they're equal or not
+}
